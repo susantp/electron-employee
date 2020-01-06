5908465dccd8f1d518619affe10a3a0aba518b12
Sudip Adhikari <sudip.adhikari01@gmail.com>
2020-01-06 13:56:50 +0545

    employee profile inserted

--- windows/add/addEmployee.js
+++ windows/add/addEmployee.js
@@ -1,16 +1,50 @@
-const { remote, ipcRenderer } = require("electron");
-var mysql = require("mysql");
+const { ipcRenderer, dialog } = require("electron");
+var fs = require("fs");
+var os = require("os");
+const connection = require("./../../service/db-connection");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: null,
-  database: "crupee-salary-meter"
-});
+// when submit button clicked
+var empDetailsBtn = document.getElementById("empDetailsBtn");
+empDetailsBtn.addEventListener("click", event => {
+  event.preventDefault();
+  var fullname = document.getElementById("fullname").value;
+  var joined_date = document.getElementById("joinedDate").value;
+  var basic_salary = document.getElementById("basicSalary").value;
+  var food_allowance = document.getElementById("foodAllowance").value;
+  var fuel_allowance = document.getElementById("fuelAllowance").value;
+  var insurance = document.getElementById("insurance").value;
+  var pf = document.getElementById("pf").value;
+  var contact = document.getElementById("contact").value;
+  var dob = document.getElementById("dob").value;
+  var designation = document.getElementById("designation").value;
+  var email = document.getElementById("email").value;
+  var address = document.getElementById("address").value;
+  // var uploadFilePath = document.getElementById("uploadFile").value;
 
-connection.connect(function(err) {
-  if (err) {
-    console.log(err.code);
-    console.log(err.fatal);
-  }
+  var uploadFilePath = "sudip.jpg";
+
+  $query =
+    "INSERT INTO employee_profile (name,image,joined_date,food_allowance,fuel_allowance,insurance,basic_salary,provident_fund,contact_number,dob,designation,email,address) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+  connection.query(
+    $query,
+    [
+      fullname,
+      uploadFilePath,
+      joined_date,
+      food_allowance,
+      fuel_allowance,
+      insurance,
+      basic_salary,
+      pf,
+      contact,
+      dob,
+      designation,
+      email,
+      address
+    ],
+    (error, rows, field) => {
+      if (error) console.log(error.message);
+      console.log(rows);
+    }
+  );
 });
