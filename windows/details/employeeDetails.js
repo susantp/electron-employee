e6d31f401c667c8984d46663cce3b5495d1abb4f
Susant Paudel <crupee.susant@gmail.com>
2020-01-10 16:09:32 +0545

    put default image and package update

--- windows/details/employeeDetails.js
+++ windows/details/employeeDetails.js
@@ -1,25 +1,32 @@
 const { ipcRenderer } = require("electron");
 const connection = require("./../../service/db-connection");
-const app = require('electron').remote.app;
+const app = require("electron").remote.app;
 ipcRenderer.on("employee-id", (event, id) => {
   console.log("id from employee details", id);
-  $query = `SELECT * FROM employee_profile WHERE id=?`;
+  let $query = `SELECT * FROM employee_profile WHERE id=?`;
 
   connection.query($query, [id], (erro, rows, field) => {
-    console.log("the clicked row is ", rows);
     ipcRenderer.send("selected-employee-details", rows);
-    console.log(rows[0].name);
-
     document.getElementById("fullname").innerHTML = rows[0].name;
     document.getElementById("pm").innerHTML = rows[0].designation;
     document.getElementById("joinedDate").innerHTML = rows[0].joined_date;
-    document.getElementById("profImg").src = app.getAppPath()+"/windows/add/images/"+rows[0].image;
+    if (
+      rows[0].image === "undefined" ||
+      rows[0].image === "" ||
+      rows[0].image === null
+    ) {
+      document.getElementById("profImg").src = __dirname + "/placeholder.png";
+    } else {
+      document.getElementById("profImg").src =
+        app.getAppPath() + "/windows/add/images/" + rows[0].image;
+    }
   });
-  var selectedElement = document.getElementById("select");
+
+  let selectedElement = document.getElementById("select");
 
   selectedElement.addEventListener("change", function(event) {
     event.preventDefault();
-    var selectedYear =
+    let selectedYear =
       selectedElement.options[selectedElement.selectedIndex].value;
     console.log("selected value is " + selectedYear);
   });
