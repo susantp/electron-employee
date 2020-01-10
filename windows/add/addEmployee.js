30fcd30975a610caa0d9993ed729e85ef193bf0f
Susant Paudel <crupee.susant@gmail.com>
2020-01-10 14:50:50 +0545

    file upload task completed

--- windows/add/addEmployee.js
+++ windows/add/addEmployee.js
@@ -1,31 +1,32 @@
 const { ipcRenderer, dialog } = require("electron");
-const app = require("electron").remote.app;
-var fs = require("fs");
-const http = require("http");
+const fs = require("fs");
 const connection = require("./../../service/db-connection");
+const os = require("os");
 
 // when submit button clicked
-var empDetailsBtn = document.getElementById("empDetailsBtn");
+let empDetailsBtn = document.getElementById("empDetailsBtn");
 const submitForm = document.querySelector("#form");
+let uploadedFilePath = null;
+let fileElement = null;
 submitForm.addEventListener("submit", event => {
   event.preventDefault();
-  var fullname = document.getElementById("fullname").value;
-  var joined_date = document.getElementById("joinedDate").value;
-  var basic_salary = document.getElementById("basicSalary").value;
-  var food_allowance = document.getElementById("foodAllowance").value;
-  var fuel_allowance = document.getElementById("fuelAllowance").value;
-  var insurance = document.getElementById("insurance").value;
-  var pf = document.getElementById("pf").value;
-  var contact = document.getElementById("contact").value;
-  var dob = document.getElementById("dob").value;
-  var designation = document.getElementById("designation").value;
-  var email = document.getElementById("email").value;
-  var address = document.getElementById("address").value;
-  var filePath = document.getElementById("filetoupload").value;
-  var fileName = filePath.replace(/^.*[\\\/]/, "");
-  fs.rename(filePath, __dirname + "/images/" + fileName, error => {
+  let fullname = document.getElementById("fullname").value;
+  let joined_date = document.getElementById("joinedDate").value;
+  let basic_salary = document.getElementById("basicSalary").value;
+  let food_allowance = document.getElementById("foodAllowance").value;
+  let fuel_allowance = document.getElementById("fuelAllowance").value;
+  let insurance = document.getElementById("insurance").value;
+  let pf = document.getElementById("pf").value;
+  let contact = document.getElementById("contact").value;
+  let dob = document.getElementById("dob").value;
+  let designation = document.getElementById("designation").value;
+  let email = document.getElementById("email").value;
+  let address = document.getElementById("address").value;
+  fileElement = document.getElementById("fileToUpload");
+  let fileName = uploadedFilePath.replace(/^.*[\\\/]/, "");
+  fs.copyFile(uploadedFilePath, __dirname + "/images/" + fileName, error => {
     if (error) {
-      console.log("error occured moving image");
+      console.log(error);
     }
   });
 
@@ -55,4 +56,12 @@ submitForm.addEventListener("submit", event => {
       console.log(rows);
     }
   );
+});
+document.getElementById("fileToUpload").addEventListener("click", event => {
+  event.preventDefault();
+  ipcRenderer.send("open-file-dialog-for-file");
+  ipcRenderer.on("selected-file", (e, filePath) => {
+    // console.log(filePath);
+    uploadedFilePath = filePath;
+  });
 });
