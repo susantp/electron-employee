29a9d9933d5e466570630dcbf80321b634c30768
Sudip Adhikari <sudip.adhikari01@gmail.com>
2020-01-14 14:13:19 +0545

    function added

--- windows/details/employeeDetails.js
+++ windows/details/employeeDetails.js
@@ -89,92 +89,98 @@ $(document).ready(function(event) {
       selectedYear =
         selectedElement.options[selectedElement.selectedIndex].value;
       console.log("selected value is " + selectedYear);
+      employeeDetails();
     });
-    $quer1 = "SELECT * FROM employee_history WHERE e_id=? AND year=?";
-    if (selectedYear != "selected value is " && selectedUserId != null) {
-      connection.query(
-        $quer1,
-        [selectedUserId, selectedYear],
-        (error, rows, field) => {
-          if (error) console.log("error getting history from db ", error);
-          if (rows.length >= 1) {
-            // loop through the result
-            for (var i = 0; i < rows.length; i++) {
-              console.log("inside month ", rows[i].month);
-              // 1
-              if (rows[i].month == "1") {
-                console.log("first month is ", rows[i].month);
-                month = "January";
-                insertRow(rows[i], month);
-              }
 
-              // 2
-              else if (rows[i].month == "2") {
-                month = "February";
-                insertRow(rows[i], month);
-                console.log("second month is ", rows[i].month);
-              }
+    // employee details function
+    function employeeDetails() {
+      $quer1 = "SELECT * FROM employee_history WHERE e_id=? AND year=?";
+      if (selectedYear != "selected value is " && selectedUserId != null) {
+        connection.query(
+          $quer1,
+          [selectedUserId, selectedYear],
+          (error, rows, field) => {
+            if (error) console.log("error getting history from db ", error);
+            if (rows.length >= 1) {
+              // loop through the result
+              for (var i = 0; i < rows.length; i++) {
+                console.log("inside month ", rows[i].month);
+                // 1
+                if (rows[i].month == "1") {
+                  console.log("first month is ", rows[i].month);
+                  month = "January";
+                  insertRow(rows[i], month);
+                }
 
-              // 3
-              else if (rows[i].month == "3") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 2
+                else if (rows[i].month == "2") {
+                  month = "February";
+                  insertRow(rows[i], month);
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // 4
-              else if (rows[i].month == "4") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 3
+                else if (rows[i].month == "3") {
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // 5
-              else if (rows[i].month == "5") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 4
+                else if (rows[i].month == "4") {
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // 6
-              else if (rows[i].month == "6") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 5
+                else if (rows[i].month == "5") {
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // 7
-              else if (rows[i].month == "7") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 6
+                else if (rows[i].month == "6") {
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // 8
-              else if (rows[i].month == "8") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 7
+                else if (rows[i].month == "7") {
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // 9
-              else if (rows[i].month == "9") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 8
+                else if (rows[i].month == "8") {
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // 10
-              else if (rows[i].month == "10") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 9
+                else if (rows[i].month == "9") {
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // 11
-              else if (rows[i].month == "11") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 10
+                else if (rows[i].month == "10") {
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // 12
-              else if (rows[i].month == "12") {
-                console.log("second month is ", rows[i].month);
-              }
+                // 11
+                else if (rows[i].month == "11") {
+                  console.log("second month is ", rows[i].month);
+                }
 
-              // else
-              else {
-                console.log("month is not between 1 to 12");
+                // 12
+                else if (rows[i].month == "12") {
+                  console.log("second month is ", rows[i].month);
+                }
+
+                // else
+                else {
+                  console.log("month is not between 1 to 12");
+                }
+                // end of loop
               }
-              // end of loop
             }
           }
-        }
-      );
+        );
+      }
     }
+    // end of function
 
     test1 = () => {
       console.log(
